4aa07ef32d75af3ed7b96e97be451ab1d248cba3
Joonas Niilola <juippis@gentoo.org>
2022-03-26 10:43:55 +0200

    bin/gentoo_firefox_patch_order.sh: add usage instruction

--- bin/gentoo_firefox_patch_order.sh
+++ bin/gentoo_firefox_patch_order.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+### Reorders modified patch set numerically.
+### 
+### Usage: 
+###   cd /tmp
+###   tar xf /var/cache/distfiles/firefox-91esr-patches-06j.tar.xz
+###   cd firefox-patches/
+###   rm 00xx*.patch
+###   gentoo_firefox_patch_order.sh .
+### 
+
 i=0
 for f in $(ls *.patch | sort -u); do
     i=$((i+1));
