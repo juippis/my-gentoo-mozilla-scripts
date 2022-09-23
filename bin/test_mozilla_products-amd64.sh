28a34bde0f5fb814ed149fbc03f0b0c2de2e1134
Joonas Niilola <juippis@gentoo.org>
2022-09-23 15:58:03 +0300

    test_mozilla_products-amd64.sh: package.use sys-libs/compiler-rt[clang]

--- bin/test_mozilla_products-amd64.sh
+++ bin/test_mozilla_products-amd64.sh
@@ -12,6 +12,10 @@
 ### focus on them explicitly, and ignore 'pgo' for "regular" runs.
 ###
 
+# Recently (105+) had some clang segfaults randomly when this was not set.
+echo "sys-libs/compiler-rt clang" >> /etc/portage/package.use/compiler-rt
+echo "sys-libs/compiler-rt-sanitizers clang" >> /etc/portage/package.use/compiler-rt
+
 # With gcc, get dependencies installed right on the first run
 export USE="-clang X wayland"
 pkg-testing-tool --extra-env-file 'test.conf' --test-feature-scope never \
