cbf11d971a2a72929fb019053c19904ce35d1088
Joonas Niilola <juippis@gentoo.org>
2023-03-07 09:26:38 +0200

    test_mozilla_products-x86.sh: sync with pkg-testing-tools-0.1.2 update

--- bin/test_mozilla_products-x86.sh
+++ bin/test_mozilla_products-x86.sh
@@ -16,21 +16,21 @@ echo "sys-libs/compiler-rt-sanitizers clang" >> /etc/portage/package.use/compile
 
 # With clang, get dependencies right on the first run
 export USE="clang X wayland"
-pkg-testing-tool --extra-env-file 'test.conf' --test-feature-scope never \
-	--report /var/tmp/portage/vbslogs/mzllprdcts-clang.json \
+pkg-testing-tool --append-emerge '--autounmask=n --oneshot' --extra-env-file 'test.conf' \
+	--test-feature-scope never --report /var/tmp/portage/vbslogs/mzllprdcts-clang.json \
 	--append-required-use '!lto !pgo' --max-use-combinations 1 -p "=${1}"
 unset USE
 
 # With GCC
 export USE="-clang"
-pkg-testing-tool --extra-env-file 'test.conf' --test-feature-scope never \
-	--report /var/tmp/portage/vbslogs/mzllprdcts-gcc.json \
+pkg-testing-tool --append-emerge '--autounmask=n --oneshot' --extra-env-file 'test.conf' \
+	--test-feature-scope never --report /var/tmp/portage/vbslogs/mzllprdcts-gcc.json \
 	--append-required-use '!lto !pgo' --max-use-combinations 1 -p "=${1}"
 unset USE
 
 # With randomized USE flags
-pkg-testing-tool --extra-env-file 'test.conf' --test-feature-scope once \
-	--report /var/tmp/portage/vbslogs/mzllprdcts-misc.json \
+pkg-testing-tool --append-emerge '--autounmask=n --oneshot' --extra-env-file 'test.conf' \
+	--test-feature-scope once --report /var/tmp/portage/vbslogs/mzllprdcts-misc.json \
 	--append-required-use '!lto !pgo' --max-use-combinations 4 -p "=${1}"
 
 errors_and_qa_notices.sh
