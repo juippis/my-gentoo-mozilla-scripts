528562fa6d23e2ba3209f0938b6322c2b2f97901
Joonas Niilola <juippis@gentoo.org>
2024-05-16 08:04:57 +0300

    test_mozilla_products-x86.sh: update pkg-testing-tools parameter
     - "--max-use-combinations 1" will randomize set of use flags in 0.2.5, while
       "--max-use-combinations 0" works with default use flags.

--- bin/test_mozilla_products-x86.sh
+++ bin/test_mozilla_products-x86.sh
@@ -18,14 +18,14 @@ echo "sys-libs/compiler-rt-sanitizers clang" >> /etc/portage/package.use/compile
 export USE="clang X wayland"
 pkg-testing-tool --append-emerge '--autounmask=y --oneshot' --extra-env-file 'test.conf' \
 	--test-feature-scope never --report /var/tmp/portage/vbslogs/mzllprdcts-clang.json \
-	--append-required-use '!lto !pgo' --max-use-combinations 1 -p "=${1}"
+	--append-required-use '!lto !pgo' --max-use-combinations 0 -p "=${1}"
 unset USE
 
 # With GCC
 export USE="-clang X wayland"
 pkg-testing-tool --append-emerge '--autounmask=y --oneshot' --extra-env-file 'test.conf' \
 	--test-feature-scope never --report /var/tmp/portage/vbslogs/mzllprdcts-gcc.json \
-	--append-required-use '!lto !pgo' --max-use-combinations 1 -p "=${1}"
+	--append-required-use '!lto !pgo' --max-use-combinations 0 -p "=${1}"
 unset USE
 
 # With randomized USE flags
